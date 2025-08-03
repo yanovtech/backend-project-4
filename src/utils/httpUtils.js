7f1cb8e45a386ccc565a970480539f96da58faf4
yanovtech <yanovlive@gmail.com>
2025-08-03 11:00:07 +0300

    step 6

--- src/utils/httpUtils.js
+++ src/utils/httpUtils.js
@@ -3,33 +3,36 @@ import fsp from 'fs/promises'
 import axios from 'axios'
 import path from 'path'
 import debug from 'debug'
+import Listr from 'listr'
 
 const debugAxios = debug('axios')
 
 const downloadImg = (url, imgPath, responseType = 'arraybuffer') => {
   debugAxios(`Starting download from ${url}`)
 
-  return axios({ method: 'get', url, responseType, validateStatus: null })
-    .then((response) => {
-      if (response.status !== 200) {
-        throw new Error(`HTTP error ${response.status} while downloading resource: ${url}`)
-      }
+  return new Listr([
+    {
+      title: `Скачивание ресурса: ${url}`,
+      task: () =>
+        axios({ method: 'get', url, responseType, validateStatus: null })
+          .then((response) => {
+            if (response.status !== 200) {
+              throw new Error(`HTTP error ${response.status} while downloading resource: ${url}`)
+            }
 
-      return fsp.mkdir(path.dirname(imgPath), { recursive: true })
-        .then(() => {
-          if (responseType === 'stream') {
-            return new Promise((resolve, reject) => {
-              const writer = fs.createWriteStream(imgPath)
-              response.data.pipe(writer)
-              writer.on('finish', resolve)
-              writer.on('error', reject)
-            })
-          }
-          else {
+            if (responseType === 'stream') {
+              return new Promise((resolve, reject) => {
+                const writer = fs.createWriteStream(imgPath)
+                response.data.pipe(writer)
+                writer.on('finish', resolve)
+                writer.on('error', reject)
+              })
+            }
+
             return fsp.writeFile(imgPath, response.data)
-          }
-        })
-    })
+          }),
+    },
+  ]).run()
     .then(() => {
       debugAxios(`Successfully downloaded: ${url}`)
     })
